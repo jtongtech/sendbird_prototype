7fc0387772d2733c1880bccc7c5f6042a3afc58c
JTong <eldertong@gmail.com>
2017-11-08 17:47:24 -0500

    first real commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sendbird'
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+load './local_env.rb' if File.exist?('./local_env.rb')
+
+enable :sessions
+
+Sendbird.applications = {app_name: ENV['API_KEY']}
+Sendbird.user = ENV['username']
+Sendbird.password = ENV['password']
+Sendbird.default_app = ENV['app_name']
+
+def authorize()
+  redirect '/' if !(session[:email])
+end
+
+end
